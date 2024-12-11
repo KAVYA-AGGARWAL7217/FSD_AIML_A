34a2e6609f9077e44159f7ca45a4393d6e46358b
Kavya Aggarwal <kavyaaggarwal7217@gmail.com>
2024-12-11 09:23:08 +0530

    Ref Example

--- react-counter-app/src/App.jsx
+++ react-counter-app/src/App.jsx
@@ -2,6 +2,7 @@ import {Routes,Route} from 'react-router-dom'
 import Home from './Components/Home'
 import Counter from './Components/Counter'
 import Stopwatch from './Components/Stopwatch'
+import Refex from './Components/Refex'
 const App=()=>{
   return(
     <div>
@@ -10,6 +11,7 @@ const App=()=>{
         <Route path="/counter" element={<Counter/>}></Route>
         <Route path="/stopwatch" element={<Stopwatch/>}></Route>
         <Route path="*" element={<h1>No Page Available</h1>}></Route>
+        <Route path="/Refex" element={<Refex/>}></Route>
       </Routes>
 
     </div>
